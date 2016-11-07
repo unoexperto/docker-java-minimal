12a77d266a1a0f1732892414c817d969b26379f9
cppexpert <github@walkmind.com>
2016-11-07 13:02:34 +0300

    Minor fix  in script

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -1,2 +1,3 @@
-build.sh --local-name=docker-java-minimal --image-name=docker-java-minimal --image-owner=expert --image-tags=latest
-publish.sh --local-name=docker-java-minimal --image-name=docker-java-minimal --image-owner=expert --image-tags=latest
+#!/usr/bin/env bash
+./build.sh --local-name=docker-java-minimal --image-name=docker-java-minimal --image-owner=expert --image-tags=latest
+./publish.sh --local-name=docker-java-minimal --image-name=docker-java-minimal --image-owner=expert --image-tags=latest
