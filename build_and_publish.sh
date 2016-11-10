947ea6098de9fa24fc080b7aedf142c2c9d7fbee
cppexpert <github@walkmind.com>
2016-11-10 12:20:11 +0300

    Fixed typo

--- build_and_publish.sh
+++ build_and_publish.sh
@@ -5,7 +5,7 @@ docker login
 docker build --build-arg JAVA_VERSION_MAJOR=8 JAVA_VERSION_MINOR=112 JAVA_VERSION_BUILD=15 JAVA_PACKAGE=server-jre JAVA_SHA256_SUM=eb51dc02c1607be94249dc28b0223be3712b618ef72f48d3e2bfd2645db8b91a -t expert/docker-predictionio:server-jre .
 docker push expert/docker-predictionio:server-jre
 
-docker build --build-arg JAVA_VERSION_MAJOR=8 JAVA_VERSION_MINOR=112 JAVA_VERSION_BUILD=15 JAVA_PACKAGE=server-jre JAVA_SHA256_SUM=777bd7d5268408a5a94f5e366c2e43e720c6ce4fe8c59d9a71e2961e50d774a5 -t expert/docker-predictionio:jdk .
+docker build --build-arg JAVA_VERSION_MAJOR=8 JAVA_VERSION_MINOR=112 JAVA_VERSION_BUILD=15 JAVA_PACKAGE=jdk JAVA_SHA256_SUM=777bd7d5268408a5a94f5e366c2e43e720c6ce4fe8c59d9a71e2961e50d774a5 -t expert/docker-predictionio:jdk .
 docker push expert/docker-predictionio:jdk
 
 docker logout
